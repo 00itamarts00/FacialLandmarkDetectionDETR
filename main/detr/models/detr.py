e7fabdcb42a23cebf90d5de9bc1ac28f34e20287
00itamarts00 <itamar.tsay@gmail.com>
2021-07-04 00:58:17 +0300

    best AUC08@300W=49.5

--- main/detr/models/detr.py
+++ main/detr/models/detr.py
@@ -31,7 +31,7 @@ class DETR(nn.Module):
         self.transformer = transformer
         hidden_dim = transformer.d_model
         self.class_embed = nn.Linear(hidden_dim, num_classes + 1)
-        self.bbox_embed = MLP(hidden_dim, hidden_dim, 2, 3)
+        self.bbox_embed = MLP(hidden_dim, hidden_dim//2, 2, 3)
         self.query_embed = nn.Embedding(num_queries, hidden_dim)
         self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1)
         self.backbone = backbone
