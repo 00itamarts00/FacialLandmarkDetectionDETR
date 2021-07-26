8c0f4a0e081c71dda5c06aaca6c33bf1d41a5fa2
00itamarts00 <itamar.tsay@gmail.com>
2021-07-27 00:07:23 +0300

    added segmentation python files from detr

--- main/run_file.py
+++ main/run_file.py
@@ -4,14 +4,14 @@ import os
 import main.globals as g
 from top_level import TopLevel
 from utils.file_handler import FileHandler
-
+task_id = None
 timestamp = FileHandler.get_datetime()
 g.TIMESTAMP = timestamp
 
 fname = os.path.join(g.LOGS_PROFS, f'{g.TIMESTAMP}')
 os.makedirs(g.LOGS_PROFS, exist_ok=True)
 # TODO: add task name
-task_id = '"55077966c7074b299ab193253bc7f95d"'
+# task_id = '"55077966c7074b299ab193253bc7f95d"'
 tl = TopLevel()
 # exec_str = 'tl.single_batch_train()'
 # exec_str = f'tl.evaluate_model(task_id={task_id})'
