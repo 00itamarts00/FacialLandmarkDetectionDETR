13da87bd0359ff4702df201eb7b4a894bc2ac121
00itamarts00 <itamar.tsay@gmail.com>
2021-08-07 18:35:06 +0300

    DEBUG runs will go to DEBUG project in clearml

--- main/top_level.py
+++ main/top_level.py
@@ -33,7 +33,7 @@ class TopLevel(object):
             self.task = self.init_clearml()
 
     def init_clearml(self):
-        project_name = self.params.project
+        project_name = self.params.project if not sys.gettrace() else 'DEBUG'
         # TODO: change task name to custom value
         task_name = self.get_current_git_branch_name()
         task = Task.init(project_name, task_name, task_type='training', reuse_last_task_id=False)
