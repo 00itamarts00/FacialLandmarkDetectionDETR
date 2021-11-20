28dd1a521a7843db2f9fd7af9202c674eb2bc344
Itamar Ts <itamar.tsay@gmail.com>
2021-11-20 12:01:59 +0200

    additions no-edit

--- main/globals.py
+++ main/globals.py
@@ -1,6 +1,6 @@
 TIMESTAMP = 0
 GREEN = (0, 255, 0)
-LOGS_PROFS = '/home/itamar/thesis/outputs/logs_profiles'
+LOGS_PROFS = 'OUTPUTS/logs_profiles'
 WANDB_INIT = 0
 WANDB_API_KEY = '2a9957f6b5c14246c56ec83ca7097d151bb758d6'
 TASK_ID = 0
